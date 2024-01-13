4f715ed01164801a638f452ecbf6ebae872877f3
KartikeyNamdev <kartikeynamdev2003@gmail.com>
2024-01-13 13:01:37 +0530

    updated main.js

--- Render/main.js
+++ Render/main.js
@@ -1,20 +1,17 @@
-const ROOT=document.getElementById("root");
+const ROOT = document.getElementById("root");
 
-function startGameRenderer(data){
-    data.forEach((element) => {
-        const rowElement = document.createElement("div");
-        element.forEach((square) => {
-            const squareDiv = document.createElement("div");
-            squareDiv.classList.add(square.color);
-        });
-            
-            rowElement.classList.add("squareRow");
-            ROOT.appendChild(rowElement);
-
-
-        
-
+function startGameRenderer(data) {
+  data.forEach((element) => {
+    const rowElement = document.createElement("div");
+    element.forEach((square) => {
+      const squareDiv = document.createElement("div");
+      squareDiv.classList.add(square.color, "square");
+      rowElement.appendChild(squareDiv);
     });
+
+    rowElement.classList.add("squareRow");
+    ROOT.appendChild(rowElement);
+  });
 }
 
-export{startGameRenderer}
+export { startGameRenderer };
